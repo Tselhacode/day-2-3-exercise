036c5b0ccef68e3d4385d0e1f6934b46601d235c
Tselhacode <68340816+Tselhacode@users.noreply.github.com>
2021-07-17 14:18:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+days_left = 365*90 - (int(age)*365)
+weeks_left = 52*90 - (int(age)*52)
+months_left = 12*90 - (int(age)*12)
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+
+
+
+
+
+
+
+
